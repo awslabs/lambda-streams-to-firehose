09e762fb5af81a9c73bb80c3411ed7a79949713f
Benoit Tigeot <btigeot@octo.com>
2017-02-15 14:01:51 +0100

    Use env variable for DEBUG rather than variable inside function

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
  */
-var debug = false;
+var debug = process.env.DEBUG;
 
 var pjson = require('./package.json');
 var setRegion = process.env['AWS_REGION'];
